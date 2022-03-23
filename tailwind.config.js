e5ac3ac811d1192447f987b456cc6da49a0d0518
Francis Nwoba <frenzynwoba@gmail.com>
2022-03-23 12:32:45 +0100

    Chukwuebuka: CS-1 Onboarding Features V1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   purge: [],
-  darkMode: false, // or 'media' or 'classs'
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
