124dd19882bba63d58508dc55aef11a2e29e55cf
Francis Nwoba <frenzynwoba@gmail.com>
2022-03-23 12:39:09 +0100

    Chukwuebuka: CS-1 Onboarding Features V1

--- craco.config.js
+++ craco.config.js
@@ -5,4 +5,5 @@ module.exports = {
       },
     },
   };
+// Project Setup
   
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false, // or 'media' or 'classs'
   theme: {
     extend: {
       colors: {
